799ad2f4c42f6e621e2619e8f7f566438f6f7b4a
sinaGh-cyber <sinaina1377@gmail.com>
2021-05-06 08:28:41 +0430

    market basket python 1

---
+++ market-basket.py
@@ -0,0 +1,68 @@
+class done(Exception): pass
+marketBasket = {}
+
+
+def menu(option):
+    if option == 1:
+        add()
+    elif option == 2:
+        delItem()
+    elif option == 3:
+        showBasketContent()
+    elif option == 4:
+        searchItem()
+    elif option == 0:
+        raise done
+    else:
+        print('Eror: Enter a number between 0 and 4.')
+
+def help():
+    print(""" 
+              1. Add item
+              2. Del item
+              3. Show basket contant
+              4. Search item
+              0. Exit
+              """)
+
+
+def add():
+    item = input('Enter item: ')
+    qty = int(input('Enter Qty: '))
+    if item in marketBasket.keys():
+        marketBasket[item] += qty
+    else:
+         marketBasket[item] = qty
+
+
+def delItem():
+    item = input('Enter item to remove frome market basket: ')
+    marketBasket.pop(item, "Eror: this item dose't exist. ")
+
+
+def showBasketContent():
+    print('show baskets content:')
+    print('*'*30)
+    print(' '*30)
+    for key in marketBasket.keys():
+        print(key,' : ', marketBasket[key])
+        print(' '*30)
+        print('*'*30)
+
+
+
+def searchItem():
+    item = int(input('Enter item to search in market basket: '))
+    if item in marketBasket.keys():
+        print('found')
+    else:
+        print('not found')
+
+
+try:
+     while True:
+        help()
+        option = int(input('Enter option (0-4): '))
+        menu(option)
+except done:
+    print('sarabada!')
